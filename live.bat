437f1a4972d175773dbd4f4d96df032ae5a3f736
James Jarvis <jj@aprsworld.com>
2021-02-05 10:16:10 -0600

    missed a jar in .bat

--- live.bat
+++ live.bat
@@ -1,1 +1,1 @@
-java -cp bin;Serialio.jar;jspComm.jar;commons-text-1.9.jar;commons-lang3-3.11.jar;. RDLoggerLivePCDisplay config_default.ini
+java -cp bin;Serialio.jar;jspComm.jar;commons-text-1.9.jar;commons-lang3-3.11.jar;marineapi-0.11.0.jar;. RDLoggerLivePCDisplay config_default.ini
